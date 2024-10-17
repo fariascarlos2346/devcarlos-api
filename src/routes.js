a31a901a1af3165e1528f68bd0b8ac7894a59af4
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-17 00:42:26 -0300

    validando usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,8 @@
 import { Router } from 'express';
-
-import { v4 } from 'uuid';
-
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
- 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Carlos33',
-        email: 'fariascarlos33@email.com',
-        password_hash: 'asdfgçlkjh33'
-    });
 
-    return response.status(201).json(user);
-    
-});
+routes.post('/users', UserController.store);
 
 export default routes;
