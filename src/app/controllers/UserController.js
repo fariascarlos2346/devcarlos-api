5e61f5e7d8210b0f5f2afc2abf28fe003066fbb6
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-17 09:10:28 -0300

    validando erros de validação, mostrando todos os erros

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,7 +18,14 @@ class UserController {
             password_hash: Yup.string().min(6).required(),
             admin: Yup.boolean(),
         });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
         
+
         const { name, email, password_hash, admin } = request.body;
 
         const user = await User.create({
