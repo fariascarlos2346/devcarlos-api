c52f80c523a511e3ea1e9c180ff5e2d3fe9e35a5
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-17 23:27:38 -0300

    tabela produtos criada

--- src/database/migrations/20241018020331-create-products-table.js
+++ src/database/migrations/20241018020331-create-products-table.js
@@ -3,20 +3,41 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('products', {
+       id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+       },
+       name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+       },
+       price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+       },
+       category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+       },
+       path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+       },
+       created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+       },
+       updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+       },
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('products');
+  },
 };
