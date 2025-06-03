9973252d638d21fee3e44d7830a34b7d2e594276
Carlos Henrique <fariascarlos2346@gmail.com>
2025-06-03 10:36:31 -0300

    rota criada e mais um pacote 'UUID'

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router()
 
-routes.get('/', (require, response) => {
-    return response.status(200).json({ message: 'Hello World!, life.'})
+routes.get('/', async (require, response) => {
+    const user = await User.create({
+        name: 'Carlos Bia',
+        email: 'carlos@email.com',
+        password_hash: 'asdfdfçlkhljmç'
+    });
+
+    return response.status(201).json(user);
 })
 
 export default routes;
