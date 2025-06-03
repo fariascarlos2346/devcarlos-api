d2eddf7a2b8d1fc621e5ec33bee8285c642ddcf0
Carlos Henrique <fariascarlos2346@gmail.com>
2025-06-03 10:04:15 -0300

    campos da migrate criada

--- src/database/migrations/20250603024749-create-users-table.js
+++ src/database/migrations/20250603024749-create-users-table.js
@@ -3,20 +3,42 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+       id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+       },
+       name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+       },
+       email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+       },
+       password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+       },
+       admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+       },
+       created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+       },
+       updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+       },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface) {
+    await queryInterface.dropTable('users');
   }
 };
