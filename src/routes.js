58d46bb0e6284697581bdc81e925cd9cfaa5ade6
Carlos Henrique <fariascarlos2346@gmail.com>
2025-06-03 10:40:06 -0300

    criado usuario no formato json na port 3001

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
 
 import User from './app/models/User';
 
@@ -6,6 +7,7 @@ const routes = new Router()
 
 routes.get('/', async (require, response) => {
     const user = await User.create({
+        id: v4(),
         name: 'Carlos Bia',
         email: 'carlos@email.com',
         password_hash: 'asdfdfçlkhljmç'
